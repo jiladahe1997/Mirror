b99496622a56a411a3cac8cdf337ae28761e9ad0
mingrui.ren <mingrui.ren@bst.ai>
2024-03-22 10:40:17 +0800

    refactor: 删除注释

--- mirror_project/main/mirror.c
+++ mirror_project/main/mirror.c
@@ -24,27 +24,26 @@
 #include <wifi_provisioning/manager.h>
 #include <wifi_provisioning/scheme_ble.h>
 #include "qrcode.h"
+
+#include "mirror.h"
+
 static const char *TAG = "app";
 
 const int WIFI_CONNECTED_EVENT = BIT0;
 static EventGroupHandle_t wifi_event_group;
-
 #define PROV_QR_VERSION         "v1"
 #define PROV_TRANSPORT_SOFTAP   "softap"
 #define PROV_TRANSPORT_BLE      "ble"
 #define QRCODE_BASE_URL         "https://espressif.github.io/esp-jumpstart/qrcode.html"
 
+//blink任务
 #define STACK_SIZE 4096
 StaticTask_t xTaskBuffer;
 StackType_t xStack[ STACK_SIZE ];
-static void blink(void *);
-static void event_handler(void* arg, esp_event_base_t event_base,
-                          int32_t event_id, void* event_data);
-static void wifi_init_sta(void);
-static void get_device_service_name(char *service_name, size_t max);
-esp_err_t custom_prov_data_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen,
-                                          uint8_t **outbuf, ssize_t *outlen, void *priv_data);
-static void wifi_prov_print_qr(const char *name, const char *username, const char *pop, const char *transport);
+uint8_t blink_status;
+#define BLINK_STATUS_INITIALIZATION 0
+#define BLINK_STATUS_WIFI_CONNECTED 1
+static portMUX_TYPE blink_status_spinlock = portMUX_INITIALIZER_UNLOCKED;
 
 void app_main(void)
 {
@@ -76,60 +75,25 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 
 
-    /* Initialize Wi-Fi including netif with default config */
     esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     wifi_prov_mgr_config_t config = {
-        /* What is the Provisioning Scheme that we want ?
-         * wifi_prov_scheme_softap or wifi_prov_scheme_ble */
         .scheme = wifi_prov_scheme_ble,
-
-        /* Any default scheme specific event handler that you would
-         * like to choose. Since our example application requires
-         * neither BT nor BLE, we can choose to release the associated
-         * memory once provisioning is complete, or not needed
-         * (in case when device is already provisioned). Choosing
-         * appropriate scheme specific event handler allows the manager
-         * to take care of this automatically. This can be set to
-         * WIFI_PROV_EVENT_HANDLER_NONE when using wifi_prov_scheme_softap*/
         .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM
     };
 
-    /* Initialize provisioning manager with the
-     * configuration parameters set above */
     ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
 
     bool provisioned = false;
-    /* Let's find out if the device is provisioned */
     ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
     if (!provisioned) {
         ESP_LOGI(TAG, "Starting provisioning");
 
-        /* What is the Device Service Name that we want
-         * This translates to :
-         *     - Wi-Fi SSID when scheme is wifi_prov_scheme_softap
-         *     - device name when scheme is wifi_prov_scheme_ble
-         */
         char service_name[12];
         get_device_service_name(service_name, sizeof(service_name));
 
-        /* What is the service key (could be NULL)
-         * This translates to :
-         *     - Wi-Fi password when scheme is wifi_prov_scheme_softap
-         *          (Minimum expected length: 8, maximum 64 for WPA2-PSK)
-         *     - simply ignored when scheme is wifi_prov_scheme_ble
-         */
         const char *service_key = NULL;
-        /* This step is only useful when scheme is wifi_prov_scheme_ble. This will
-         * set a custom 128 bit UUID which will be included in the BLE advertisement
-         * and will correspond to the primary GATT service that provides provisioning
-         * endpoints as GATT characteristics. Each GATT characteristic will be
-         * formed using the primary service UUID as base, with different auto assigned
-         * 12th and 13th bytes (assume counting starts from 0th byte). The client side
-         * applications must identify the endpoints by reading the User Characteristic
-         * Description descriptor (0x2901) for each characteristic, which contains the
-         * endpoint name of the characteristic */
         uint8_t custom_service_uuid[] = {
             /* LSB <---------------------------------------
              * ---------------------------------------> MSB */
@@ -137,52 +101,29 @@ void app_main(void)
             0xea, 0x4a, 0x82, 0x03, 0x04, 0x90, 0x1a, 0x02,
         };
 
-        /* If your build fails with linker errors at this point, then you may have
-         * forgotten to enable the BT stack or BTDM BLE settings in the SDK (e.g. see
-         * the sdkconfig.defaults in the example project) */
         wifi_prov_scheme_ble_set_service_uuid(custom_service_uuid);
 
-        /* An optional endpoint that applications can create if they expect to
-         * get some additional custom data during provisioning workflow.
-         * The endpoint name can be anything of your choice.
-         * This call must be made before starting the provisioning.
-         */
         wifi_prov_mgr_endpoint_create("custom-data");
-        /* Start provisioning service */
         wifi_prov_security_t security = WIFI_PROV_SECURITY_0;
         wifi_prov_security2_params_t *sec_params = NULL;
         ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(security, (const void *) sec_params, service_name, service_key));
 
-        /* The handler for the optional endpoint created above.
-         * This call must be made after starting the provisioning, and only if the endpoint
-         * has already been created above.
-         */
         wifi_prov_mgr_endpoint_register("custom-data", custom_prov_data_handler, NULL);
 
-        /* Uncomment the following to wait for the provisioning to finish and then release
-         * the resources of the manager. Since in this case de-initialization is triggered
-         * by the default event loop handler, we don't need to call the following */
-        // wifi_prov_mgr_wait();
-        // wifi_prov_mgr_deinit();
-        /* Print QR code for provisioning */
         const char *username  = NULL;
         const char *pop = NULL;
         wifi_prov_print_qr(service_name, username, pop, PROV_TRANSPORT_BLE);
     } else {
         ESP_LOGI(TAG, "Already provisioned, starting Wi-Fi STA");
-
-        /* We don't need the manager as device is already provisioned,
-         * so let's release it's resources */
         wifi_prov_mgr_deinit();
-
-        /* Start Wi-Fi station */
         wifi_init_sta();
     }
 
     /* Wait for Wi-Fi connection */
     xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_EVENT, true, true, portMAX_DELAY);
-
+    set_blink_status(BLINK_STATUS_WIFI_CONNECTED);
     /* Start main application now */
+    
 
     return;
 
@@ -321,9 +262,16 @@ static void wifi_prov_print_qr(const char *name, const char *username, const cha
     ESP_LOGI(TAG, "If QR code is not visible, copy paste the below URL in a browser.\n%s?data=%s", QRCODE_BASE_URL, payload);
 }
 
+
+void set_blink_status(uint8_t new_blink_status) {
+    taskENTER_CRITICAL(&blink_status_spinlock);
+    blink_status = new_blink_status;
+    taskEXIT_CRITICAL(&blink_status_spinlock);
+}
+
+
 static void blink(void *){
     printf("blink start\n");
-    esp_err_t ret;
 
     //zero-initialize the config structure.
     gpio_config_t io_conf = {};
@@ -343,15 +291,22 @@ static void blink(void *){
     gpio_set_level(4, 1);  //绿
     gpio_set_level(5, 1);  //蓝
 
-    uint32_t count=0;
     while(1) {
-        count++;
-        // printf("blink count:%ld portTICK_PERIOD_MS:%ld\n", count, portTICK_PERIOD_MS);
-        //ret = gpio_set_level(3, 1);
-        ret = gpio_set_level(5, 1);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        //ret = gpio_set_level(3, 0);
-        ret = gpio_set_level(5, 0);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        switch(blink_status) {
+            //初始化，亮1秒，熄灭0.1秒
+            case BLINK_STATUS_INITIALIZATION:
+                gpio_set_level(4, 1);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
+                gpio_set_level(4, 0);
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+                break;
+            //WIFI连接成功，亮0.1秒，熄灭3秒
+            case BLINK_STATUS_WIFI_CONNECTED:
+                gpio_set_level(4, 1);
+                vTaskDelay(3000 / portTICK_PERIOD_MS);
+                gpio_set_level(4, 0);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
+                break;
+        }
     }
 }
---
+++ mirror_project/main/mirror.h
@@ -0,0 +1,16 @@
+#ifndef _MIRROR_H_
+#define _MIRROR_H_
+
+
+static void blink(void *);
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data);
+static void wifi_init_sta(void);
+static void get_device_service_name(char *service_name, size_t max);
+esp_err_t custom_prov_data_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen,
+                                          uint8_t **outbuf, ssize_t *outlen, void *priv_data);
+static void wifi_prov_print_qr(const char *name, const char *username, const char *pop, const char *transport);
+void set_blink_status(uint8_t new_blink_status);
+
+
+#endif /* _MIRROR_H_ */
